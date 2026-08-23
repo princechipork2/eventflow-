bdb6e07eb5f97630590e555a9c2cc897acd8d477
Prince-Chipork <chigyodzerterungwa@gmail.com>
2026-08-23 22:40:05 +0100

    Standardize app name as EventFlow

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = "EventHub";
+export const APP_NAME = "EventFlow";
 export const APP_TAGLINE = "Where Moments Become Events";
 export const CONTACT_EMAIL = "hello@eventhub.io";
 export const SUPPORT_EMAIL = "support@eventhub.io";
